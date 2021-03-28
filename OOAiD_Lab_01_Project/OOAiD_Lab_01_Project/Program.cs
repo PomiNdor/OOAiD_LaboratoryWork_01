76362d8219325799bab35f94c370e1f60d34d305
PomiNdor <shishkov-011@bk.ru>
2021-03-28 19:49:24 +0300

    Init rep and create project

---
+++ OOAiD_Lab_01_Project/OOAiD_Lab_01_Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOAiD_Lab_01_Project
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Привет мир! Меня зовут Данил Шишков!");
+    }
+  }
+}
